5ace125f259ca429be196a79a934ccea38531a23
Renz Carl Supnet <31431868+renzsupnet@users.noreply.github.com>
2021-04-20 02:48:35 -0700

    Updated server.js to use an environment variable:
    Initialized favoriteFood variable to use environment variable FAVORITE_FOOD and log it into console.

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ async function main() {
   while(true) {
     console.log("Microservices rock!");
     await sleep(5000);
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log("My favorite food is "+favoriteFood);
   }
 }
 
